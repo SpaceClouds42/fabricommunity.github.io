7b3af95edccd300a0c53502fcfd3e5877dbc13fc
gdude2002 <gdude2002@gmail.com>
2021-03-05 13:05:32 +0000

    Update gh-pages to output generated at 1f38cbf

--- static/static/js/prism.js
+++ static/static/js/prism.js
@@ -1,5 +1,5 @@
 /* PrismJS 1.23.0
-https://prismjs.com/download.html#themes=prism-okaidia&languages=markup+css+clike+javascript+bash+batch+clojure+diff+git+groovy+ignore+java+javadoc+javadoclike+javastacktrace+js-extras+json+json5+kotlin+markdown+properties+scala+toml+yaml&plugins=line-numbers+highlight-keywords+normalize-whitespace */
+https://prismjs.com/download.html#themes=prism-okaidia&languages=markup+css+clike+javascript+bash+batch+clojure+diff+git+groovy+ignore+java+javadoc+javadoclike+javastacktrace+js-extras+json+json5+kotlin+markdown+properties+scala+toml+twig+yaml&plugins=line-numbers+highlight-keywords+normalize-whitespace */
 /// <reference lib="WebWorker"/>
 
 var _self = (typeof window !== 'undefined')
@@ -3200,6 +3200,53 @@ delete Prism.languages.scala['function'];
         'punctuation': /[.,=[\]{}]/
     };
 }(Prism));
+
+Prism.languages.twig = {
+    'comment': /\{#[\s\S]*?#\}/,
+    'tag': {
+        pattern: /\{\{[\s\S]*?\}\}|\{%[\s\S]*?%\}/,
+        inside: {
+            'ld': {
+                pattern: /^(?:\{\{-?|\{%-?\s*\w+)/,
+                inside: {
+                    'punctuation': /^(?:\{\{|\{%)-?/,
+                    'keyword': /\w+/
+                }
+            },
+            'rd': {
+                pattern: /-?(?:%\}|\}\})$/,
+                inside: {
+                    'punctuation': /.+/
+                }
+            },
+            'string': {
+                pattern: /("|')(?:\\.|(?!\1)[^\\\r\n])*\1/,
+                inside: {
+                    'punctuation': /^['"]|['"]$/
+                }
+            },
+            'keyword': /\b(?:even|if|odd)\b/,
+            'boolean': /\b(?:true|false|null)\b/,
+            'number': /\b0x[\dA-Fa-f]+|(?:\b\d+(?:\.\d*)?|\B\.\d+)(?:[Ee][-+]?\d+)?/,
+            'operator': [
+                {
+                    pattern: /(\s)(?:and|b-and|b-xor|b-or|ends with|in|is|matches|not|or|same as|starts with)(?=\s)/,
+                    lookbehind: true
+                },
+                /[=<>]=?|!=|\*\*?|\/\/?|\?:?|[-+~%|]/
+            ],
+            'property': /\b[a-zA-Z_]\w*\b/,
+            'punctuation': /[()\[\]{}:.,]/
+        }
+    },
+
+    // The rest can be parsed as HTML
+    'other': {
+        // We want non-blank matches
+        pattern: /\S(?:[\s\S]*\S)?/,
+        inside: Prism.languages.markup
+    }
+};
 
 (function (Prism) {
 
