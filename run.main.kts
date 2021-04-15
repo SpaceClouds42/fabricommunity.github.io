aae04120b8e4b7dd3558cf28cb54457ef187ed55
SpaceClouds42 <spaceclouds42@gmail.com>
2021-04-15 19:18:29 -0400

    added program argument handlers

--- run.main.kts
+++ run.main.kts
@@ -1,28 +1,34 @@
 @file:Repository("https://dl.bintray.com/jakubriegel/kotlin-shell")
 @file:DependsOn("eu.jrie.jetbrains:kotlin-shell-core:0.2.1")
 
-val isWindows = System.getProperty("os.name").toLowerCase().indexOf("win") >= 0
+var buildCommand = "./build.sh"
+var assetsCommand = "./build-assets.sh"
+var serveCommand = "./serve.sh"
 
-if (isWindows) {
-    println("Generating pages...")
-    Runtime.getRuntime().exec("cmd /c start \"\" build.bat").waitFor()
-
-    println("Generating assets...")
-    Runtime.getRuntime().exec("cmd /c start \"\" build-assets.bat").waitFor()
-
-    println("Running server...")
-    val serve = Runtime.getRuntime().exec("cmd /c start \"\" serve.bat")
-    println("Server Online at http://localhost:8080/")
-    serve.waitFor()
-} else {
-    println("Generating pages...")
-    Runtime.getRuntime().exec("./build.sh").waitFor()
-
-    println("Generating assets...")
-    Runtime.getRuntime().exec("./build-assets.sh").waitFor()
-
-    println("Running server...")
-    val serve = Runtime.getRuntime().exec("./serve.sh")
-    println("Server Online at http://localhost:8080/")
-    serve.waitFor()
+if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) {
+    buildCommand = "cmd /c start \"\" build.bat"
+    assetsCommand = "cmd /c start \"\" build-assets.bat"
+    serveCommand = "cmd /c start \"\" serve.bat"
 }
+
+if (!args.contains("skip-pages")) {
+    println("Generating pages...")
+    Runtime.getRuntime().exec(buildCommand).waitFor()
+} else {
+    println("Skipped page generation.")
+}
+
+if (!args.contains("skip-assets")) {
+    println("Generating assets...")
+    Runtime.getRuntime().exec(assetsCommand).waitFor()
+} else {
+    println("Skipped asset generation.")
+}
+
+println("Running server...")
+val serve: Process = Runtime.getRuntime().exec(serveCommand)
+println("Server Online at http://localhost:8080/")
+serve.waitFor()
+
+// Prevents printing the exit code
+System.out.close()
